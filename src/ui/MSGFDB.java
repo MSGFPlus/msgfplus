8d22e875467f3ecc63fcf0e92f5fa47eb81f1126
Sangtae Kim <sak008@eng.ucsd.edu>
2011-12-09 01:55:22 +0000

    Bugs related to multiple mods per residue have been fixed.

--- src/ui/MSGFDB.java
+++ src/ui/MSGFDB.java
@@ -45,8 +45,8 @@ import msutil.Spectrum;
 import msutil.SpectrumAccessorBySpecIndex;
 
 public class MSGFDB {
-	public static final String VERSION = "6886";
-	public static final String RELEASE_DATE = "12/07/2011";
+	public static final String VERSION = "6946";
+	public static final String RELEASE_DATE = "12/08/2011";
 	
 	public static final String DECOY_DB_EXTENSION = ".revConcat.fasta";
 	public static void main(String argv[])
