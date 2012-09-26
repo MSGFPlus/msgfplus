d24b5d35281fe2c929a6da60a764850837e0c942
Sangtae Kim <sak008@eng.ucsd.edu>
2012-09-26 23:36:28 +0000

    MS2 parser now parses MS2 format defined by MacCoss

--- src/main/java/edu/ucsd/msjava/parser/MS2SpectrumParser.java
+++ src/main/java/edu/ucsd/msjava/parser/MS2SpectrumParser.java
@@ -18,6 +18,9 @@ import edu.ucsd.msjava.msutil.SpectrumMetaInfo;
  */
 public class MS2SpectrumParser implements SpectrumParser {
 
+	private Spectrum spec = null;
+	private Boolean isSpecSorted = null;
+	
 	/**
 	 * Reads a spectrum from ms2 file and returns it.
 	 * @param lineReader A LineReader object points to the start of a spectrum.
@@ -25,54 +28,84 @@ public class MS2SpectrumParser implements SpectrumParser {
 	 */
 	public Spectrum readSpectrum(LineReader lineReader) 
 	{
-		Spectrum spec = null;
-
-		boolean sorted = true;
 		float prevMass = 0;
 		String buf;
+
+		do
+		{
+			buf = lineReader.readLine();
+		}
+		while(buf != null && buf.startsWith("H"));
+		if(buf == null)
+			return null;
+		
+		if(buf.startsWith("S"))
+		{
+			String[] token = buf.split("\\s+");
+			spec = new Spectrum();
+			int startScanNum = Integer.parseInt(token[1]);
+			int endScanNum = Integer.parseInt(token[2]);
+			float precursorMz = Float.parseFloat(token[3]);
+			spec = new Spectrum(precursorMz, 0, 0);
+			spec.setStartScanNum(startScanNum);
+			spec.setEndScanNum(endScanNum);
+			isSpecSorted = true;
+		}
+		else if(spec == null)
+			return null;
+			
 		while((buf = lineReader.readLine()) != null)
 		{
 			String[] token = buf.split("\\s+");
-			if(buf.startsWith(":"))	// start of a spectrum
+			if(buf.startsWith("H"))
+				continue;
+			else if(buf.startsWith("S"))	// start of a next spectrum
 			{
-				token = buf.substring(1).split("\\.");
-				int startScanNum = Integer.parseInt(token[0]);
-				int endScanNum = Integer.parseInt(token[1]);
+				Spectrum specCopy = spec;
+				Boolean isSpecSortedCopy = isSpecSorted;
 				
-				buf = lineReader.readLine();
-				if(buf != null)
-				{
-					token = buf.split("\\s+");
-					float MH = Float.parseFloat(token[0]);
-					int charge = Integer.parseInt(token[1]);
-					float precursorMz = ((MH-(float)Composition.PROTON)+charge*(float)Composition.PROTON)/charge;
-					float precursorIntensity = 0;
-	                spec = new Spectrum(precursorMz, charge, precursorIntensity);
-	                spec.setStartScanNum(startScanNum);
-	                spec.setEndScanNum(endScanNum);
-				}
+				spec = new Spectrum();
+				int startScanNum = Integer.parseInt(token[1]);
+				int endScanNum = Integer.parseInt(token[2]);
+				float precursorMz = Float.parseFloat(token[3]);
+				spec = new Spectrum(precursorMz, 0, 0);
+				spec.setStartScanNum(startScanNum);
+				spec.setEndScanNum(endScanNum);
+				isSpecSorted = true;
+				
+				if(!isSpecSortedCopy)
+					Collections.sort(specCopy);
+				return specCopy;
             }
+			else if(buf.startsWith("Z"))
+			{
+				int charge = Integer.parseInt(token[1]);
+				float precursorMH = Float.parseFloat(token[2]);
+				float precursorMz = ((precursorMH-(float)Composition.PROTON)+charge*(float)Composition.PROTON)/charge;
+				spec.setPrecursor(new Peak(precursorMz, 0, charge));
+			}
 			else if(token.length == 2)	// a peak
 			{
                 assert(spec != null);
                 float mass = Float.parseFloat(token[0]);
-                if(sorted && mass < prevMass)
-                	sorted = false;
+                if(isSpecSorted && mass < prevMass)
+                	isSpecSorted = false;
                 else
                 	prevMass = mass;
                 float intensity = Float.parseFloat(token[1]);
                 spec.add(new Peak(mass, intensity, 1));
 			}
-  			else 	// end of a spectrum
-  			{
-  				if(spec != null)
-  				{
-  	  				if(!sorted)
-  	  					Collections.sort(spec);
-  					return spec;
-  				}
-  			}
 		}
+		
+		if(spec != null)
+		{
+			if(!isSpecSorted)
+				Collections.sort(spec);
+			Spectrum specCopy = spec;
+			spec = null;
+			return specCopy;
+		}
+
 		return spec;			
 	}
 	
@@ -87,28 +120,27 @@ public class MS2SpectrumParser implements SpectrumParser {
 		String buf;
 		long offset = 0;
 		int specIndex = 0;
+		
+		SpectrumMetaInfo metaInfo = null;
 		while((buf = lineReader.readLine()) != null)
 		{
-			if(buf.startsWith(":"))
+			if(buf.startsWith("S"))	// scan
 			{
 				specIndex++;
 				
-				SpectrumMetaInfo metaInfo = new SpectrumMetaInfo();
+				metaInfo = new SpectrumMetaInfo();
 				metaInfo.setPosition(offset);
 				metaInfo.setID("index=" + (specIndex-1));
 				
-				buf = lineReader.readLine();
-				if(buf != null)
+				String[] token = buf.split("\\s+");
+				if(token.length < 4)
 				{
-					String[] token = buf.split("\\s+");
-					float MH = Float.parseFloat(token[0]);
-					int charge = Integer.parseInt(token[1]);
-					float precursorMz = ((MH-(float)Composition.PROTON)+charge*(float)Composition.PROTON)/charge;
-					metaInfo.setPrecursorMz(precursorMz);
+					System.err.println("Illegal ms2 file format!");
+					System.exit(-1);
 				}
-
+				float precursorMz = Float.parseFloat(token[3]);
+				metaInfo.setPrecursorMz(precursorMz);
 				specIndexMap.put(specIndex, metaInfo);
-				
 			}
 			
 			offset = lineReader.getPosition();
@@ -118,29 +150,41 @@ public class MS2SpectrumParser implements SpectrumParser {
 	
 	public static void test() throws Exception
 	{
-		String fileName = System.getProperty("user.home")+"/Research/ToolDistribution/RefTest/SpecFormatTest/Yufeng_8LTQ005_010_F3a_dta.ms2";
-	    SpectraIterator iterator = new SpectraIterator(fileName, new MS2SpectrumParser());
-	    int numSpecs = 0;
-	    while(iterator.hasNext())
-	    {
-	    	Spectrum spec = iterator.next();
-	    	numSpecs++;
-	    	System.out.println(spec.getPrecursorPeak().getMz()+" "+spec.getCharge()+" "+spec.getSpecIndex()+" "+spec.getScanNum());
-	    	if(numSpecs > 5)
-	    		break;
-	    }
-	    System.out.println("NumSpectra: " + numSpecs);
-	    
+		String fileName = System.getProperty("user.home")+"/Research/Data/QCShew/QC_Shew_12_02_2_1Aug12_Cougar_12-06-11.ms2";
+		
+	    java.util.Map<Integer, Float> specIndexPrecursorMzMap = new java.util.HashMap<Integer,Float>();
+	    int numSpecs;
+
 	    numSpecs = 0;
 	    SpectraMap map = new SpectraMap(fileName, new MS2SpectrumParser());
+	    
 	    for(int specIndex : map.getSpecIndexList())
 	    {
 	    	Spectrum spec = map.getSpectrumBySpecIndex(specIndex);
 	    	numSpecs++;
-	    	System.out.println(spec.getPrecursorPeak().getMz()+" "+spec.getCharge()+" "+spec.getSpecIndex()+" "+spec.getScanNum());
-	    	if(numSpecs > 5)
-	    		break;
+	    	specIndexPrecursorMzMap.put(spec.getSpecIndex(), spec.getPrecursorPeak().getMz());
 	    }
+	    System.out.println("NumSpectra: " + numSpecs);
+	    
+//	    Spectrum scan87 = map.getSpectrumBySpecIndex(79);
+//	    System.out.println("**** " + scan87.getPrecursorPeak().getMz()+" "+scan87.getPrecursorPeak().getCharge());
+	    
+	    numSpecs = 0;
+	    SpectraIterator iterator = new SpectraIterator(fileName, new MS2SpectrumParser());
+	    while(iterator.hasNext())
+	    {
+	    	Spectrum spec = iterator.next();
+	    	numSpecs++;
+	    	
+	    	Float precursorMz = specIndexPrecursorMzMap.get(spec.getSpecIndex());
+//	    	System.out.println(spec.getPrecursorPeak().getMz()+" "+spec.getCharge()+" "+spec.getSpecIndex()+" "+spec.getScanNum());
+	    	if(precursorMz == null || precursorMz != spec.getPrecursorPeak().getMz())
+	    	{
+	    		System.out.println(precursorMz + " != " + spec.getPrecursorPeak().getMz());
+		    	System.exit(0);
+	    	}
+	    }
+
 	    System.out.println("NumSpectra: " + numSpecs);
 	}
 	
