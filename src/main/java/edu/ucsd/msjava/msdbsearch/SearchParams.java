fbc8905be9b8f8fbdeeb874ade36dc9cc06f94a7
Sangtae Kim <sak008@eng.ucsd.edu>
2012-10-25 17:49:26 +0000

    Multiple spectrum file bug fix

--- src/main/java/edu/ucsd/msjava/msdbsearch/SearchParams.java
+++ src/main/java/edu/ucsd/msjava/msdbsearch/SearchParams.java
@@ -199,7 +199,6 @@ public class SearchParams {
 					dbSearchIOList.add(new DBSearchIOFiles(f, specFormat, outputFile));
 				}
 			}
-			return null;
 		}
 		
 		// DB file
--- src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
+++ src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
@@ -39,8 +39,8 @@ import edu.ucsd.msjava.params.ParamManager;
 
 
 public class MSGFPlus {
-	public static final String VERSION = "Beta (v8730)";
-	public static final String RELEASE_DATE = "10/16/2012";
+	public static final String VERSION = "Beta (v8777)";
+	public static final String RELEASE_DATE = "10/25/2012";
 	
 	public static final String DECOY_DB_EXTENSION = ".revCat.fasta";
 	public static final String DECOY_PROTEIN_PREFIX = "XXX";
