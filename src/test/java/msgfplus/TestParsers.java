6d73bfacc89379d899637a450bb67024c1761edd
Sangtae Kim <sak008@eng.ucsd.edu>
2012-10-27 01:08:13 +0000

    Added Ms2ToMgf (no testing)

---
+++ src/test/java/msgfplus/TestParsers.java
@@ -0,0 +1,22 @@
+package msgfplus;
+
+import java.io.File;
+
+import org.junit.Test;
+
+import edu.ucsd.msjava.misc.ParamToTxt;
+
+public class TestParsers {
+	@Test
+	public void testParamToTxt()
+	{
+//		File paramFile = new File("/Users/kims336/Research/Data/TrainingITRAQ/Global/params/HCD_HighRes_Tryp_iTRAQ.param");
+//		ParamToTxt.convert(paramFile);
+	}
+	
+	@Test
+	public void ms2ToMgfTest()
+	{
+		File ms2File = new File("");
+	}
+}
