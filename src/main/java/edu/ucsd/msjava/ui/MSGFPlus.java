200ab0b8eaa6f4a98ee6dc57969a0d9e0372e3ed
Sangtae Kim <sak008@eng.ucsd.edu>
2013-06-03 18:05:39 +0000

    v9630

--- src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
+++ src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
@@ -40,8 +40,8 @@ import edu.ucsd.msjava.sequences.Constants;
 
 
 public class MSGFPlus {
-	public static final String VERSION = "Beta (v9600)";
-	public static final String RELEASE_DATE = "05/10/2013";
+	public static final String VERSION = "Beta (v9630)";
+	public static final String RELEASE_DATE = "05/31/2013";
 	
 	public static final String DECOY_DB_EXTENSION = ".revCat.fasta";
 	public static final String DECOY_PROTEIN_PREFIX = "XXX";
