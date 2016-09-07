bf5773cc3641fa8d674ea92a6bad3c8a95ca2d35
alchemistmatt <matthew.monroe@pnnl.gov>
2016-09-07 15:35:00 -0700

    Update version

--- src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
+++ src/main/java/edu/ucsd/msjava/ui/MSGFPlus.java
@@ -21,8 +21,8 @@ import java.util.logging.Logger;
 
 
 public class MSGFPlus {
-    public static final String VERSION = "Release (v2016.08.31)";
-    public static final String RELEASE_DATE = "8/31/2016";
+    public static final String VERSION = "Release (v2016.09.07)";
+    public static final String RELEASE_DATE = "9/07/2016";
 
     public static final String DECOY_DB_EXTENSION = ".revCat.fasta";
     public static final String DECOY_PROTEIN_PREFIX = "XXX";
