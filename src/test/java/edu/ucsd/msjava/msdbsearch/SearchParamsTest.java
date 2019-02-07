ab8ed64229b9f0c18814b64c0b45fe0cc7a31621
yperez <ypriverol@gmail.com>
2019-02-07 16:30:19 +0000

    first interation of configuration file

---
+++ src/test/java/edu/ucsd/msjava/msdbsearch/SearchParamsTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+/**
+ * This code is licensed under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * ==Overview==
+ *
+ * @author ypriverol on 07/02/2019.
+ */
+public class SearchParamsTest {
+
+}
