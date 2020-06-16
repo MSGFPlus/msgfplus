c5fa52dde2a3128050f3c1675969b0f0c919c49b
alchemistmatt <matthew.monroe@pnnl.gov>
2020-06-16 14:10:13 -0700

    Check for null values to avoid a null pointer exception

--- src/main/java/edu/ucsd/msjava/params/DoubleParameter.java
+++ src/main/java/edu/ucsd/msjava/params/DoubleParameter.java
@@ -17,8 +17,16 @@ public class DoubleParameter extends NumberParameter<Double> {
     public String parse(String value) {
         try {
             super.value = Double.valueOf(value);
+
+            if (minValue == null)
+                minValue = Double.NEGATIVE_INFINITY;
+
+            if (maxValue == null)
+                maxValue = Double.POSITIVE_INFINITY;
+
             String range = (super.isMinInclusive ? "[" : "(") + minValue + "," + maxValue + (super.isMaxInclusive ? "]" : ")");
-            if (this.value < minValue || this.value > maxValue
+            if (this.value < minValue ||
+                    this.value > maxValue
                     || !super.isMinInclusive && this.value.equals(minValue)
                     || !super.isMaxInclusive && this.value.equals(maxValue))
                 return "must be in the range " + range;
